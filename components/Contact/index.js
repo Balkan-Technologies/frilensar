5900214d271203e325ada4d8900b15a5150538a1
raul vladau <alurduvaal@rauls-iMac.local>
2020-08-04 18:15:28 +0300

    initial commit

---
+++ components/Contact/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Layout from '../Layout';
+import ContactForm from './ContactForm';
+
+function ContactPage(props){
+  return (
+    <Layout>
+      <ContactForm />
+    </Layout>
+  );
+}
+
+export default ContactPage;
---
+++ pages/contact.js
@@ -0,0 +1,11 @@
+import App from '../components/App'
+import Contact from '../components/Contact/';
+
+const IndexPage = () => (
+  <App>
+    <Contact />
+  </App>
+)
+
+export default IndexPage;
+
